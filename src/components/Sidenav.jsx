e70d5494a1a5dc6e5e069fc91ed59551075aa97d
Cole Nyi Nyi <colenyinyi@Coles-MacBook-Air.local>
2024-01-17 20:21:58 -0800

    tailwind syntax fixes

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -13,15 +13,15 @@ const Sidenav = () => {
         {
             nav==true ? (
                 <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-                    <a href="#main" className='w-[50%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadown-gray-400 m-2 p-4 cusor-pointer hover:scale-110 ease-in duration-200'>
+                    <a href="#main" className='w-[50%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineHome size={20}/>
                         <span className='pl-4'>Home</span>
                     </a>
-                    <a href="#projects" className='w-[50%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadown-gray-400 m-2 p-4 cusor-pointer hover:scale-110 ease-in duration-200'>
+                    <a href="#projects" className='w-[50%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineDesktop size={20}/>
-                        <span className='pl-4'>Projects</span>
+                        <span className='pl-4'>Work</span>
                     </a>
-                    <a href="#about" className='w-[50%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadown-gray-400 m-2 p-4 cusor-pointer hover:scale-110 ease-in duration-200'>
+                    <a href="#about" className='w-[50%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <BsPerson size={20}/>
                         <span className='pl-4'>About</span>
                     </a>
